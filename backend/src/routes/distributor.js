36e5555839b81666c7f6794331a29d0c5b0a096d
Lorran Sutter <sutterlorran@gmail.com>
2020-06-14 18:31:29 -0400

    Validation middleware

--- backend/src/routes/distributor.js
+++ backend/src/routes/distributor.js
@@ -1,6 +1,8 @@
 const express = require('express');
 
 const distributor_controller = require('../controllers/distributorController');
+const distributor_validator = require('../middlewares/distributorValidator');
+const { validate } = require('../middlewares/validate');
 
 const routes = express.Router();
 
@@ -8,8 +10,8 @@ routes.get('/', distributor_controller.index);
 
 routes.get('/index', distributor_controller.index);
 
-routes.post('/new', distributor_controller.create);
+routes.post('/new', distributor_validator.registration, validate, distributor_controller.create);
 
-routes.post('/login', distributor_controller.login);
+routes.post('/login', distributor_validator.login, validate, distributor_controller.login);
 
 module.exports = routes;
