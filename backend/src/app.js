51479e00db5148b04901ae373f281a9b95c673cf
Lorran Sutter <sutterlorran@gmail.com>
2020-06-14 20:48:00 -0400

    Remove unecessary comments

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,6 +1,5 @@
 const express = require('express');
 const createError = require('http-errors');
-// const cookieParser = require('cookie-parser');
 const compression = require('compression');
 const cors = require('cors');
 
@@ -11,10 +10,9 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-// app.use(cookieParser());
 app.use(compression());
+
 app.use('/distributors', distributor);
-// app.use('/uploads', express.static(path.resolve(__dirname, '..', 'uploads')));
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
