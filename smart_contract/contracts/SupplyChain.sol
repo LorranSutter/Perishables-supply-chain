b77155df40a64df5249f8cf1bc3a4adc49b8c149
Lorran Sutter <sutterlorran@gmail.com>
2020-06-17 21:04:14 -0400

    Battery lifecycle tests

--- smart_contract/contracts/SupplyChain.sol
+++ smart_contract/contracts/SupplyChain.sol
@@ -10,7 +10,7 @@ contract SupplyChain is ERC721Token, AccessControl {
     using Address for address;
     using SafeMath for uint256;
 
-    bytes32 public constant FACTORY_ROLE = keccak256("FACTORY_ROLE");
+    bytes32 public constant MANUFACTURER_ROLE = keccak256("MANUFACTURER_ROLE");
     bytes32 public constant TRANSPORT_ROLE = keccak256("TRANSPORT_ROLE");
     bytes32 public constant DISTRIBUTOR_ROLE = keccak256("DISTRIBUTOR_ROLE");
 
@@ -23,13 +23,13 @@ contract SupplyChain is ERC721Token, AccessControl {
     struct Battery {
         uint256 id;
         string manufacturer;
-        string serialno;
-        uint256 thermal;
+        bytes6 serialno;
+        int16 thermal;
         address currentOwner;
-        string location;
-        uint256 time;
+        bytes25 location; // latitude -90.00000000 longitude -180.00000000
     }
 
+    // mapping(address => mapping(uint256 => Battery)) public distributorToBattery;
     mapping(uint256 => Battery) public batteries;
     uint256 public nextId;
 
@@ -37,13 +37,9 @@ contract SupplyChain is ERC721Token, AccessControl {
         admin = msg.sender;
     }
 
-    event ManufacturerAdded(address _manufacturer);
-    event TransporterAdded(address _transporter);
-    event DistributorAdded(address _distributor);
-
     modifier onlyManufacturer() {
         require(
-            hasRole(FACTORY_ROLE, msg.sender),
+            hasRole(MANUFACTURER_ROLE, msg.sender),
             "Caller is not a Manufacturer"
         );
         _;
@@ -65,24 +61,29 @@ contract SupplyChain is ERC721Token, AccessControl {
         _;
     }
 
+    modifier onlyCurrentOwner(uint256 _id) {
+        require(
+            msg.sender == batteries[_id].currentOwner,
+            "Caller is not a the Current Owner"
+        );
+        _;
+    }
+
     //Add manufacturer
     function addManufacturer(address manufacturer) public {
         require(msg.sender == admin, "only admin");
-        _setupRole(FACTORY_ROLE, manufacturer);
-        emit ManufacturerAdded(manufacturer);
+        _setupRole(MANUFACTURER_ROLE, manufacturer);
     }
 
     //Add transporter
     function addTransporter(address transporter) public {
         require(msg.sender == admin, "only admin");
         _setupRole(TRANSPORT_ROLE, transporter);
-        emit TransporterAdded(transporter);
     }
 
     function addDistributor(address distributor) public {
         require(msg.sender == admin, "only admin");
         _setupRole(DISTRIBUTOR_ROLE, distributor);
-        emit DistributorAdded(distributor);
     }
 
     //Distributor Deposit money to order the battery by next function
@@ -111,63 +112,55 @@ contract SupplyChain is ERC721Token, AccessControl {
     //mint Battery function (onlyManufacturer)
     function _makeBattery(
         string memory _manufacturer,
-        string memory _serialno,
-        uint256 _thermal,
-        string memory _location
+        bytes6 _serialno,
+        int16 _thermal,
+        bytes25 _location
     ) public onlyManufacturer() {
+        _mint(msg.sender, nextId);
         batteries[nextId] = Battery(
             nextId,
             _manufacturer,
             _serialno,
             _thermal,
             msg.sender,
-            _location,
-            now
+            _location
         );
-        _mint(msg.sender, nextId);
         nextId++;
     }
 
     //Only manufacturer Role
     function _startTransport(
         uint256 _id,
-        uint256 _thermal,
         address _adsTransport,
-        string memory _location
-    ) public onlyManufacturer() {
+        int16 _thermal,
+        bytes25 _location
+    ) public onlyManufacturer() onlyCurrentOwner(_id) {
         batteries[_id].currentOwner = _adsTransport;
         batteries[_id].thermal = _thermal;
         batteries[_id].location = _location;
-        batteries[_id].time = now;
         _transfer(msg.sender, _adsTransport, _id);
     }
 
-    //Just to check the value
-    function getBattery(uint256 _id)
-        public
-        view
-        returns (
-            string memory,
-            uint256,
-            address
-        )
-    {
-        return (
-            batteries[_id].manufacturer,
-            batteries[_id].thermal,
-            batteries[_id].currentOwner
-        );
+    function _changeTransporter(
+        uint256 _id,
+        address _newTransporter,
+        int16 _thermal,
+        bytes25 _location
+    ) public onlyTransporter() onlyCurrentOwner(_id) {
+        batteries[_id].currentOwner = _newTransporter;
+        batteries[_id].thermal = _thermal;
+        batteries[_id].location = _location;
+        _transfer(msg.sender, _newTransporter, _id);
     }
 
     //Only transporter
     function _thermalMonitor(
         uint256 _id,
-        string memory _location,
-        uint256 _thermal
-    ) public onlyTransporter() {
+        int16 _thermal,
+        bytes25 _location
+    ) public onlyTransporter() onlyCurrentOwner(_id) {
         batteries[_id].thermal = _thermal;
         batteries[_id].location = _location;
-        batteries[_id].time = now;
         _transfer(msg.sender, msg.sender, _id);
     }
 
@@ -175,13 +168,61 @@ contract SupplyChain is ERC721Token, AccessControl {
     function _endofTransportation(
         uint256 _id,
         address _distAddress,
-        string memory _location,
-        uint256 _thermal
-    ) public onlyTransporter() {
+        int16 _thermal,
+        bytes25 _location
+    ) public onlyTransporter() onlyCurrentOwner(_id) {
         batteries[_id].currentOwner = _distAddress;
         batteries[_id].thermal = _thermal;
         batteries[_id].location = _location;
-        batteries[_id].time = now;
         _transfer(msg.sender, _distAddress, _id);
     }
+
+    //Just to check the value
+    function getBatteryThermal(uint256 _id)
+        public
+        view
+        onlyDistributor()
+        returns (int16)
+    {
+        return (batteries[_id].thermal);
+    }
+
+    function getBatteryLocation(uint256 _id)
+        public
+        view
+        onlyDistributor()
+        returns (bytes25)
+    {
+        return (batteries[_id].location);
+    }
+
+    function getBatteryCurrentOwner(uint256 _id)
+        public
+        view
+        onlyDistributor()
+        returns (address)
+    {
+        return (batteries[_id].currentOwner);
+    }
+
+    // //Just to check the value
+    // function getBatteryInfo(uint256 _id)
+    //     public
+    //     view
+    //     returns (
+    //         string memory,
+    //         bytes6,
+    //         int16,
+    //         address,
+    //         bytes25
+    //     )
+    // {
+    //     return (
+    //         batteries[_id].manufacturer,
+    //         batteries[_id].serialno,
+    //         batteries[_id].thermal,
+    //         batteries[_id].currentOwner,
+    //         batteries[_id].location
+    //     );
+    // }
 }
--- smart_contract/test/SupplyChain.spec.js
+++ smart_contract/test/SupplyChain.spec.js
@@ -1,13 +1,23 @@
+const crypto = require('crypto');
 const BigNumber = require("bignumber.js");
 const truffleAssert = require("truffle-assertions");
+
+const randomGenerator = require('./randomGenerator');
 
 const SupplyChain = artifacts.require("SupplyChain");
 
 contract("Contract Test", (accounts) => {
+    const addressZero = "0x0000000000000000000000000000000000000000";
     const admin = accounts[0];
     const manufacturer = accounts[1];
-    const transporter = accounts[2];
-    const distributor = accounts[3];
+    const transporter1 = accounts[2];
+    const transporter2 = accounts[3];
+    const distributor = accounts[4];
+
+    const MANUFACTURER_ROLE = "0xeefb95e842a3287179d933b4460be539a1d5af11aa8b325bb45c5c8dc92de4ed";
+    const TRANSPORT_ROLE = "0xb82a77b83aa57abb9bcfbacbdfb3201de26ecd2b35d0d6cf6c3b9bb2cb7026c4";
+    const DISTRIBUTOR_ROLE = "0xfbd454f36a7e1a388bd6fc3ab10d434aa4578f811acbbcf33afb1c697486313c";
+
     let contractInstance;
 
     // Deploy a new contract before each test to prevent one test from interfering with the other
@@ -15,126 +25,168 @@ contract("Contract Test", (accounts) => {
         contractInstance = await SupplyChain.new('', { from: admin });
     });
 
-    it('should be able to add manufacturer', async () => {
-        const tx = await contractInstance.addManufacturer(manufacturer, { from: admin });
+    describe('01 - Add roles', () => {
+        it('A - should be able to add manufacturer', async () => {
+            const tx = await contractInstance.addManufacturer(manufacturer, { from: admin });
 
-        truffleAssert.eventEmitted(tx, "ManufacturerAdded", (obj) => {
-            return (
-                obj._manufacturer === manufacturer
+            truffleAssert.eventEmitted(tx, "RoleGranted", (obj) => {
+                return (
+                    obj.role == MANUFACTURER_ROLE &&
+                    obj.account === manufacturer &&
+                    obj.sender == admin
+                )
+            }, 'Fail RoleGranted event');
+        });
+
+        it('B - should be able to add transporter', async () => {
+            const tx = await contractInstance.addTransporter(transporter1, { from: admin });
+
+            truffleAssert.eventEmitted(tx, "RoleGranted", (obj) => {
+                return (
+                    obj.role == TRANSPORT_ROLE &&
+                    obj.account === transporter1 &&
+                    obj.sender == admin
+                )
+            }, 'Fail RoleGranted event');
+        });
+
+        it('C - should be able to add distributor', async () => {
+            const tx = await contractInstance.addDistributor(distributor, { from: admin });
+
+            truffleAssert.eventEmitted(tx, "RoleGranted", (obj) => {
+                return (
+                    obj.role == DISTRIBUTOR_ROLE &&
+                    obj.account === distributor &&
+                    obj.sender == admin
+                )
+            }, 'Fail RoleGranted event');
+        });
+    })
+
+    describe('02 - Battery life cycle', () => {
+        it('A - should mint a new battery token', async () => {
+            await contractInstance.addManufacturer(manufacturer, { from: admin });
+
+            const tx = await contractInstance._makeBattery(
+                randomGenerator.randomString(10),
+                crypto.randomBytes(6),
+                randomGenerator.randomNumber(10, 20),
+                crypto.randomBytes(25),
+                { from: manufacturer }
             )
-        }, 'Fail ManufacturerAdded Event');
-    });
 
-    it('should be able to add transporter', async () => {
-        const tx = await contractInstance.addTransporter(transporter, { from: admin });
+            truffleAssert.eventEmitted(tx, "Transfer", (obj) => {
+                return (
+                    obj._from === addressZero &&
+                    obj._to === manufacturer &&
+                    new BigNumber(0).isEqualTo(obj._tokenId)
+                )
+            }, 'Fail Transfer in battery token minting');
+        });
 
-        truffleAssert.eventEmitted(tx, "TransporterAdded", (obj) => {
-            return (
-                obj._transporter === transporter
+        it('B - should be able to transfer battery token to a transporter', async () => {
+            await contractInstance.addManufacturer(manufacturer, { from: admin });
+            await contractInstance.addTransporter(transporter1, { from: admin });
+
+            await contractInstance._makeBattery(
+                randomGenerator.randomString(10),
+                crypto.randomBytes(6),
+                randomGenerator.randomNumber(10, 20),
+                crypto.randomBytes(25),
+                { from: manufacturer }
             )
-        }, 'Fail TransporterAdded Event');
-    });
 
-    it('should be able to add distributor', async () => {
-        const tx = await contractInstance.addDistributor(distributor, { from: admin });
-
-        truffleAssert.eventEmitted(tx, "DistributorAdded", (obj) => {
-            return (
-                obj._distributor === distributor
+            const tx = await contractInstance._startTransport(
+                0,
+                transporter1,
+                randomGenerator.randomNumber(10, 20),
+                crypto.randomBytes(25),
+                { from: manufacturer }
             )
-        }, 'Fail DistributorAdded Event');
+
+            truffleAssert.eventEmitted(tx, "Transfer", (obj) => {
+                return (
+                    obj._from === manufacturer &&
+                    obj._to === transporter1 &&
+                    new BigNumber(0).isEqualTo(obj._tokenId)
+                )
+            }, 'Fail Transfer in battery token transfer from manufacturer to transporter');
+        });
+
+        it('C - should be able to transfer battery token to another transporter', async () => {
+            await contractInstance.addManufacturer(manufacturer, { from: admin });
+            await contractInstance.addTransporter(transporter1, { from: admin });
+            await contractInstance.addTransporter(transporter2, { from: admin });
+
+            await contractInstance._makeBattery(
+                randomGenerator.randomString(10),
+                crypto.randomBytes(6),
+                randomGenerator.randomNumber(10, 20),
+                crypto.randomBytes(25),
+                { from: manufacturer }
+            )
+
+            await contractInstance._startTransport(
+                0,
+                transporter1,
+                randomGenerator.randomNumber(10, 20),
+                crypto.randomBytes(25),
+                { from: manufacturer }
+            )
+
+            const tx = await contractInstance._changeTransporter(
+                0,
+                transporter2,
+                randomGenerator.randomNumber(10, 20),
+                crypto.randomBytes(25),
+                { from: transporter1 }
+            )
+
+            truffleAssert.eventEmitted(tx, "Transfer", (obj) => {
+                return (
+                    obj._from === transporter1 &&
+                    obj._to === transporter2 &&
+                    new BigNumber(0).isEqualTo(obj._tokenId)
+                )
+            }, 'Fail Transfer in battery token transfer from transporter 1 to transporter 2');
+        });
+
+        it('D - should be able to transfer battery token from transporter to distributor', async () => {
+            await contractInstance.addManufacturer(manufacturer, { from: admin });
+            await contractInstance.addTransporter(transporter1, { from: admin });
+            await contractInstance.addDistributor(distributor, { from: admin });
+
+            await contractInstance._makeBattery(
+                randomGenerator.randomString(10),
+                crypto.randomBytes(6),
+                randomGenerator.randomNumber(10, 20),
+                crypto.randomBytes(25),
+                { from: manufacturer }
+            )
+
+            await contractInstance._startTransport(
+                0,
+                transporter1,
+                randomGenerator.randomNumber(10, 20),
+                crypto.randomBytes(25),
+                { from: manufacturer }
+            )
+
+            const tx = await contractInstance._endofTransportation(
+                0,
+                distributor,
+                randomGenerator.randomNumber(10, 20),
+                crypto.randomBytes(25),
+                { from: transporter1 }
+            )
+
+            truffleAssert.eventEmitted(tx, "Transfer", (obj) => {
+                return (
+                    obj._from === transporter1 &&
+                    obj._to === distributor &&
+                    new BigNumber(0).isEqualTo(obj._tokenId)
+                )
+            }, 'Fail Transfer in battery token transfer from transporter to distributor');
+        });
     });
-
-    // it('should return correct patient holder', async () => {
-    //     await contractInstance.addPatient(patientName, dateBirth, hospitalEmergency, { from: hospital1 });
-
-    //     const holderAddress = await contractInstance.patientHolder(1);
-    //     assert.equal(holderAddress, hospital1, `Patient 1 holder should be ${hospital1}`);
-    // });
-
-    // it('should add a new patient', async () => {
-    //     const tx = await contractInstance.addPatient(patientName, dateBirth, hospitalEmergency, { from: hospital1 })
-
-    //     truffleAssert.eventEmitted(tx, "Transfer", (obj) => {
-    //         return (
-    //             obj._from === hospital1 &&
-    //             obj._to === hospital1 &&
-    //             new BigNumber(1).isEqualTo(obj._tokenId)
-    //         )
-    //     }, 'Fail addPatient');
-    // });
-
-    // it('should transfer a patient from a hospital to another', async () => {
-    //     const tx1 = await contractInstance.addPatient(patientName, dateBirth, hospitalEmergency, { from: hospital1 })
-
-    //     truffleAssert.eventEmitted(tx1, "Transfer", (obj) => {
-    //         return (
-    //             obj._from === hospital1 &&
-    //             obj._to === hospital1 &&
-    //             new BigNumber(1).isEqualTo(obj._tokenId)
-    //         );
-    //     }, 'Fail addPatient');
-
-    //     let holderAddress = await contractInstance.ownerOf(1);
-    //     assert.equal(holderAddress, hospital1, `Hospital should be ${hospital1}`);
-
-    //     const tx2 = await contractInstance.transferPatient(hospital2, 1, { from: hospital1 });
-
-    //     truffleAssert.eventEmitted(tx2, "Transfer", (obj) => {
-    //         return (
-    //             obj._from === hospital1 &&
-    //             obj._to === hospital2 &&
-    //             new BigNumber(1).isEqualTo(obj._tokenId)
-    //         );
-    //     }, 'Fail addPatient');
-
-    //     holderAddress = await contractInstance.ownerOf(1);
-    //     assert.equal(holderAddress, hospital2, `Hospital should be ${hospital2}`);
-    // });
-
-    // it('should return patient info', async () => {
-    //     const tx1 = await contractInstance.addPatient(patientName, dateBirth, hospitalEmergency, { from: hospital1 });
-
-    //     truffleAssert.eventEmitted(tx1, "Transfer", (obj) => {
-    //         return (
-    //             obj._from === hospital1 &&
-    //             obj._to === hospital1 &&
-    //             new BigNumber(1).isEqualTo(obj._tokenId)
-    //         );
-    //     }, 'Fail addPatient');
-
-    //     const nameReturn = await contractInstance.getPatientName(1, { from: hospital1 });
-    //     assert.equal(nameReturn, patientName, `Patient name should be ${patientName}`);
-
-    //     const dateBirthReturn = await contractInstance.getPatientDateBirth(1, { from: hospital1 });
-    //     assert.equal(dateBirthReturn, dateBirth, `Patient date birth should be ${dateBirth}`);
-
-    //     const statusReturn = await contractInstance.getPatientStatus(1, { from: hospital1 });
-    //     assert(new BigNumber(statusReturn).isEqualTo(0), 'Patient status should be 0');
-
-    //     const emergencyReturn = await contractInstance.getPatientEmergencyHospital(1, { from: hospital1 });
-    //     assert.equal(emergencyReturn, hospitalEmergency, `Patient emergency hospital address should be ${hospitalEmergency}`);
-    // });
-
-    // it('should move critical patients to emergency hospital', async () => {
-    //     const tx1 = await contractInstance.addPatient(patientName, dateBirth, hospitalEmergency, { from: hospital1 });
-
-    //     truffleAssert.eventEmitted(tx1, "Transfer", (obj) => {
-    //         return (
-    //             obj._from === hospital1 &&
-    //             obj._to === hospital1 &&
-    //             new BigNumber(1).isEqualTo(obj._tokenId)
-    //         );
-    //     }, 'Fail addPatient');
-
-    //     const tx2 = await contractInstance.movePatientToCritical(1, { from: hospital1 });
-
-    //     truffleAssert.eventEmitted(tx2, "Transfer", (obj) => {
-    //         return (
-    //             obj._from === hospital1 &&
-    //             obj._to === hospitalEmergency &&
-    //             new BigNumber(1).isEqualTo(obj._tokenId)
-    //         );
-    //     }, `Fail transfer patient from ${hospital1} to ${hospitalEmergency}`);
-    // });
 });
